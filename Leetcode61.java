9b32707e0265ba80d057fbbc2f9598003fdb4aa7
nayan malviya <naymalviya@gmail.com>
2024-12-07 21:46:22 +0530

    07/12/2024

--- Leetcode61.java
+++ Leetcode61.java
@@ -7,12 +7,35 @@ class Leetcode61 {
         head.next.next.next.next = new ListNode(5);
 
         System.out.println("[" + head + "]");
-        System.out.println(rotateRight(head, 0));
+        System.out.println(rotateRight(head, 2));
     }
 
-    static int rotateRight(ListNode head, int k) {
+    static ListNode rotateRight(ListNode head, int k) {
+        int length = 0;
+
+        ListNode temp = head;
+        while(temp != null){
+            length++;
+            temp = temp.next;
+        }
+
+        if(length<2 || k == 0 || k==length)return head;
+
+        k = k%length;
+        ListNode prev = null;
+        temp = head;
+        int i = 1;
+        while(i <= length-k && temp != null){
+            i++;
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = null;
+        prev = temp;
+        while(prev.next != null)prev = prev.next;
+        prev.next = head;
         
-        return 1;
+        return temp;
     }
 
 }
@@ -32,9 +55,9 @@ class Listcode {
         this.val = val;
         this.next = next;
     }
-    
+
     @Override
     public String toString() {
-        return val + ", " + next; 
+        return val + ", " + next;
     }
 }
