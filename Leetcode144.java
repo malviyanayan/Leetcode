5dee708f6586bcc19aa8b4190fecc9448609df7c
nayan malviya <naymalviya@gmail.com>
2024-12-18 22:58:20 +0530

    18/11/2024

---
+++ Leetcode144.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+class Leetcode144{
+    public static void main(String[] args) {
+        
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+    private void dfs(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        result.add(node.val); // Visit root
+        dfs(node.left, result); // Traverse left subtree
+        dfs(node.right, result); // Traverse right subtree
+    }
+}
