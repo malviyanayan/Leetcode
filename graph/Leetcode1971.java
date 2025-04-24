78e889bbb261f65d72c04d0dfd0bd37aa7df0024
nayan malviya <naymalviya@gmail.com>
2025-04-24 10:36:33 +0530

    24/04/2025

--- graph/Leetcode1971.java
+++ graph/Leetcode1971.java
@@ -1,10 +1,29 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class Leetcode1971{
+
+    static List<List<Integer>> edgesToAdjacencyList(int[][] edges,int n){
+        List<List<Integer>> list = new ArrayList<>(n);
+
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<edges.length;i++){
+            list.get(edges[i][0]).add(edges[i][1]);
+            list.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        return list;
+    }
     
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Queue<Integer> queue = new LinkedList<>();
+        List<List<Integer>> list = edgesToAdjacencyList(edges, n);
+        
         boolean[] visited = new boolean[n];
         queue.add(source);
 
@@ -13,8 +32,7 @@ class Leetcode1971{
             
             if(visited[next])continue;
             visited[next] = true;
-            for(int temp : edges[next]){
-                System.out.print(temp+",");
+            for(int temp : list.get(next)){
                 if(temp == destination)return true;
                 queue.add(temp);
             }
---
+++ graph/Leetcode200.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Leetcode200 {
+
+    static void bfs(char[][] grid,Queue<int[]> queue){
+        while(!queue.isEmpty()){
+            int[] next = queue.remove();
+            int i=next[0],j = next[1];
+            
+            grid[i][j] = 'x';
+            if((j+1) < grid[0].length && grid[i][j+1] == '1')queue.add(new int[]{i, j+1});
+            if((i+1) < grid.length && grid[i+1][j] == '1')queue.add(new int[]{i+1, j});
+            if((i-1) >= 0 && grid[i-1][j] == '1')queue.add(new int[]{i-1, j});
+            if((j-1) >= 0 && grid[i][j-1] == '1')queue.add(new int[]{i, j-1});
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        Queue<int[]>queue = new LinkedList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    queue.add(new int[]{i, j});
+                    bfs(grid, queue);
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+
+
+    // ############## Improved ###############
+    static void bfs1(char[][] grid,Queue<int[]> queue){
+        while(!queue.isEmpty()){
+            int[] next = queue.remove();
+            int i=next[0],j = next[1];
+            if(grid[i][j] == 'x' || grid[i][j] == '0')continue;
+            grid[i][j] = 'x';
+            if((j+1) < grid[0].length)queue.add(new int[]{i, j+1});
+            if((i+1) < grid.length)queue.add(new int[]{i+1, j});
+            if((i-1) >= 0)queue.add(new int[]{i-1, j});
+            if((j-1) >= 0)queue.add(new int[]{i, j-1});
+        }
+    }
+
+    public int numIslands1(char[][] grid) {
+        int count = 0;
+        Queue<int[]>queue = new LinkedList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    queue.add(new int[]{i, j});
+                    bfs(grid, queue);
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }    
+}
